804be942991fc524d3f7a8d2ecaf1716675c951b
sunhughees <sunhughees@sina.cn>
2016-03-01 00:01:47 +0800

    modified:   ../src_js/hello.js 	new file:   nl01.newlisp

--- src_js/hello.js
+++ src_js/hello.js
@@ -7,3 +7,13 @@ console.log("Hello, World!")
 console.log(one_array)
 
 console.log(x)
+
+var person = {
+	firstName:"John",
+	lastName:"Doe",
+	age:50,
+	eyeColor:"blue"
+};
+
+console.log(person.eyeColor);
+console.log(person.fullName);
